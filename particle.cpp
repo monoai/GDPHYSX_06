1ea9c866a2810706fb53f911dbac6c686d31cfed
monoAI <46586720+monoai@users.noreply.github.com>
2022-10-13 17:26:09 +0800

    Code cleanup
    Removed some unused codes and added a few additional comments for documentation purposes

--- particle.cpp
+++ particle.cpp
@@ -5,31 +5,32 @@
 #include "obj_mesh.h"
 
 particle::particle(glm::mat4* transMat, GLuint* normalTransformLoc, GLuint* modelTransformLoc, ObjData object, float &x, float &y, float &z) : trans(*transMat), normalTrans(*normalTransformLoc), modelTrans(*modelTransformLoc) {
+	// Assign class variables
 	this->xPos = x;
 	this->yPos = y;
 	this->zPos = z;
+
+	// The function to apply the transformations, values are set outside of the constructor.
 	applyTrans();
+
+	// Applying textures to the object if there are any detected.
 	glActiveTexture(GL_TEXTURE0);
 	GLuint objTexture = object.textures[object.materials[0].diffuse_texname];
 	glBindTexture(GL_TEXTURE_2D, objTexture);
 
 	glDrawElements(GL_TRIANGLES, object.numFaces, GL_UNSIGNED_INT, (void*)0);
 
-	//unbindtexture after rendering
+	// unbindtexture after rendering
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void particle::applyTrans() {
 	this->trans = glm::mat4(1.0f);
+	// [DEBUG]
 	//std::cout << "currxPos set: " << this->xPos << "x: " << x << std::endl;
 	//std::cout << "curryPos set: " << yPos << "y: " << y << std::endl;
-	//*transMat = glm::translate(*transMat, glm::vec3(x,y,zPos));
 	setTranslate();
-	//this->trans = *transMat;
-	//this->trans = glm::translate(this->trans, glm::vec3(xPos,yPos,zPos));
-	//setTranslate(this->xPos,this->yPos,this->zPos);
-	//*trans = glm::rotate(*trans, glm::radians(0.0f), glm::vec3(0.0f,0.0f,0.0f));
-	//this->trans = glm::scale(this->trans, glm::vec3(1.0f,1.0f,1.0f));
+	//this->trans = glm::rotate(this->trans, glm::radians(0.0f), glm::vec3(0.0f,0.0f,0.0f));
 	this->trans = glm::scale(this->trans, glm::vec3(1.0f,1.0f,1.0f));
 	
 	glm::mat4 normalTransLoc = glm::transpose(glm::inverse(this->trans));
@@ -44,8 +45,9 @@ void particle::setPosition(float &x, float &y, float &z) {
 	this->xPos += x;
 	this->yPos += y;
 	this->zPos += z;
-	std::cout << "xPos set: " << xPos << std::endl;
-	std::cout << "yPos set: " << yPos << std::endl;
+	// [DEBUG]
+	//std::cout << "xPos set: " << xPos << std::endl;
+	//std::cout << "yPos set: " << yPos << std::endl;
 }
 
 void particle::setTranslate() {
